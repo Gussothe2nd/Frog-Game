0b7d4d64ffd511943bff4938cdbf955f208fe4cf
TrajceskiIII <118051662+TrajceskiIII@users.noreply.github.com>
2023-01-18 21:54:17 +1000

    laser no worky

---
+++ FrogGameGameEditable/Assets/LoadClass2ndTry.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Security.Cryptography;
+
+public class LoadClass2ndTry : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    public Transform spawnPoint;
+    //public TMP_Text label;
+
+    void Start()
+    {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        GameObject prefab = characterPrefabs[selectedCharacter];
+        GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+        //label.text = prefab.name;
+    }
+}
