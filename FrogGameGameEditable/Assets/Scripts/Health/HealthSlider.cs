7a90cd470681721e8db77fb6f8053a6e183d2cab
Gussothe2nd <bluewhale861@gmail.com>
2023-01-24 08:10:14 +1000

    HealthBarUniversal progression

---
+++ FrogGameGameEditable/Assets/Scripts/Health/HealthSlider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSlider : MonoBehaviour
+{
+
+    [SerializeField]
+    private Health _health;
+
+    [SerializeField]
+    private RectTransform _barRect;
+
+    [SerializeField]
+    private RectMask2D _mask;
+
+    [SerializeField]
+    private TMP_Text _hpIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
